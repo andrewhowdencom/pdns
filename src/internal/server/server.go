a8836b5657c730b72fc2ae0df0ceb69e6d1aca9e
Andrew Howden <hello@andrewhowden.com>
2019-09-14 10:41:51 +0200

    Functional prototype of the proxy
    This commit introduces a functional prototype of the proxy that just
    forwards the responses to the DNS over TLS proxy
    
    == Design Notes
    
    === Initial Spec
    
    Initially the reading of the project requirements lead the author to
    understand that the DNS queries should be unpacked an represented in an
    in-software primitive (struct), before being reserialized and forwarded
    on to the intermediary proxy.
    
    That still seems the best implementation path as it allows introspection
    of the requests and adding of a caching layer; both early requirements
    of most DNS systems.
    
    However, reimplementing the DNS spec proved expensive in terms of time.
    Given this, the minimal implementation has been created that adheres to
    the requirements.
    
    == Known Bugs
    
    === Additional Padding
    
    The CloudFlare DNS over TLS resolver is returning an incorrect length
    prefix for responses, giving additional return for responses that do
    not support it.
    
    See https://l.littleman.co/2LTRyjK

--- src/internal/server/server.go
+++ src/internal/server/server.go
@@ -2,14 +2,14 @@ package server
 
 import (
 	"bufio"
+	"crypto/tls"
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"net"
 	"time"
 
 	log "github.com/sirupsen/logrus"
-
-	"go.pkg.andrewhowden.com/pdns/internal/dns/question"
 )
 
 const defaultListenPort int = 53
@@ -36,28 +36,16 @@ func Serve() error {
 
 		// Handle the connection. Each connection is handled in its own goroutine and assumed to deal with the
 		// connection within that context. This allows multiple connections to be executed in parallel
-		go handle(connection)
+		go proxy(connection)
 	}
 }
 
-// handle reads the connection looking for the complete DNS packet before passing it off to the assembler
-//
-// In DNS, the message is not terminated with an EOL character but rather a fixed max length (UDP) or a prefix to the
-// message that indicates how long the message will be (TCP).
-//
-// In this TCP implementation the prefix is
-//
-// See https://tools.ietf.org/html/rfc1035#section-4.2.2
-//
-// @todo: Figure out better error handling. DNS should have a defined error return format
-func handle(connection net.Conn) {
-	prefix := make([]byte, 2)
-	reader := bufio.NewReader(connection)
-
+func proxy(inConn net.Conn) {
+	// Set up the connection
 	// The recommendation from the RFC 1035 is a 2m timeout duration. See:
 	// - https://tools.ietf.org/html/rfc1035#section-4.2.2
 	timeout, _ := time.ParseDuration("2m")
-	err := connection.SetReadDeadline(time.Now().Add(timeout))
+	err := inConn.SetReadDeadline(time.Now().Add(timeout))
 
 	if err != nil {
 		log.WithFields(log.Fields{
@@ -67,33 +55,75 @@ func handle(connection net.Conn) {
 		return
 	}
 
-	// Read the prefix for message length
-	_, err = reader.Read(prefix)
+	conf := &tls.Config{}
+	outConn, err := tls.Dial("tcp", "8.8.8.8:853", conf)
+
 	if err != nil {
 		log.WithFields(log.Fields{
 			"error": err.Error(),
-		}).Error("unable to read the length prefix")
+		}).Error("unable to connect to upstream resolver")
+	}
 
-		return
+	defer outConn.Close()
+
+	// Query the upstream
+	query, err := read(inConn, true)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Error("unable to unpack incoming message")
+	}
+
+	// Testing
+	length := make([]byte, 2)
+	binary.BigEndian.PutUint16(length, uint16(len(query)))
+
+	_, err = outConn.Write(append(length, query...))
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Error("failed to write to upstream tls socket")
+	}
+
+	// Calculate response length
+	response, err := read(outConn, true)
+	binary.BigEndian.PutUint16(length, uint16(len(response)))
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Error("failed to read back from connection")
+	}
+
+	inConn.Write(append(length, response...))
+}
+
+// read extracts the query byte array from a connection object, such as an incoming connection
+func read(conn net.Conn, hasLengthPrefix bool) ([]byte, error) {
+	if hasLengthPrefix == false {
+		return []byte{}, errors.New("reading non length prefixed responses is currently not supported")
+	}
+
+	prefix := make([]byte, 2)
+	reader := bufio.NewReader(conn)
+
+	// Read the prefix for message length
+	_, err := reader.Read(prefix)
+	if err != nil {
+		return []byte{}, fmt.Errorf("unable to read the length prefix: %s", err.Error())
 	}
 
 	// Get the length of the record
-	length := int(binary.BigEndian.Uint16(prefix[:]))
+	length := int(binary.BigEndian.Uint16(prefix))
 
 	// Read the body
 	payload := make([]byte, length)
 	_, err = reader.Read(payload)
 
 	if err != nil {
-		log.WithFields(log.Fields{
-			"error": err.Error(),
-		}).Error("unable to read data from connection")
-
-		return
+		return []byte{}, fmt.Errorf("unable to read data from connection: %s", err.Error())
 	}
 
-	fmt.Printf("%x\n", payload)
-
-	// Just close the connection
-	connection.Close()
+	return payload, nil
 }
